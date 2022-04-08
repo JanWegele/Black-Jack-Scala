8f93057555c9cd50743a9919f9e8bd8039f381c9
JanWegele <jan.wegele1@gmail.com>
2022-04-08 08:27:56 +0200

    Update BlackJackScala

--- src/main/scala/BlackJack.scala
+++ src/main/scala/BlackJack.scala
@@ -1,6 +1,6 @@
 @main def hello(): Unit =
   println("!!This is BlackJack!!")
-  println(mesh(25, 4))
+  println(mesh)
 
 val eo1: String = sys.props("line.separator")
 
@@ -12,76 +12,76 @@ def space() = " "
 
 //-------------------------------------------------/Table
 //Table width
-def widthCalc(width: Int): Int =
-  if (width < 10) {
+def width(width: Int): Int =
+  if(width<10){
     widthDefault
 
   } else {
-    if (width % 2 == 0) {
-      width
+    if(width%2 == 0){
+      widthDefault
     } else {
-      width + 1
+      width+1
     }
   }
 
-//Table length
-def lengthCalc(length: Int): Int =
-  if (length < 0 || length > 10) {
-    lengthDefault
-  } else {
-    length
-  }
+
+val widthParam: Int = width(15)
 
 
 //Table default methods
 def tableWidth(): String =
   "+" + ("-" * widthDefault) + "+" + eo1
 def cells(): String =
-  "|" + space() * widthDefault + "|" + eo1
+  "|" + space()*widthDefault + "|" + eo1
 
 
 //Table method overloading
 def tableWidth(width: Int): String =
-  val tWidth: Int = widthCalc(width)
-  "+" + ("-" * tWidth) + "+" + eo1
+  "+" + ("-" * width) + "+" + eo1
 def cells(width: Int): String =
-  val tWidth: Int = widthCalc(width)
-  "|" + space() * tWidth + "|" + eo1
+  "|" + space()*width + "|" + eo1
+
+
+//Table length
+def length(length: Int): Int =
+  if(length<0){
+    lengthDefault
+  }else {
+    length
+  }
+
+val lengthParam: Int = length(0)
 
 
 //-------------------------------------------------/Card
 //making card scalable with table size
 def widthForCard(width: Int): Int =
-  val cWidth: Int = widthCalc(width)
-  ((cWidth - (widthCard + 2)) * 0.5).toInt
+  ((width-(widthCard+2))*0.5).toInt
 
 
 //Card size should not be scalable
-def cardWidth(): String =
-  "+" + ("-" * widthCard) + "+"
-def cardLength(): String =
-  "|" + (space() * widthCard) + "|"
+val cardWidth: String =
+  "+" + ("-"*widthCard) + "+"
+val cardLength: String =
+  "|" + (space()*widthCard) + "|"
+
 
 
 //-------------------------------------------------/Card and Table
 //Fitting card on the table
 def cardTotalWidth(width: Int): String =
-  val widthTotal: Int = widthCalc(width)
-  "|" + (space() * widthForCard(widthTotal)) + cardWidth() + (space() * widthForCard(widthTotal)) + "|" + eo1
+  "|" + (space()*widthForCard(width)) + cardWidth + (space()*widthForCard(width)) + "|" + eo1
 
 def cardTotalHeight(width: Int): String =
-  val widthTotal: Int = widthCalc(width)
-  "|" + (space() * widthForCard(widthTotal)) + cardLength() + (space() * widthForCard(widthTotal)) + "|" + eo1
+  "|" + (space()*widthForCard(width)) + cardLength + (space()*widthForCard(width)) + "|" + eo1
 
 def cardTotal(width: Int): String =
-  val widthTotal: Int = widthCalc(width)
-  cardTotalWidth(widthTotal) + cardTotalHeight(widthTotal) * 2 + cardTotalWidth(widthTotal)
+  cardTotalWidth(width) + cardTotalHeight(width)*2 + cardTotalWidth(width)
+
+
 
 
 //-------------------------------------------------/Mesh
 //Putting Table and Card together
-def mesh(width: Int, lengthT: Int): String =
-  tableWidth(width) + cardTotal(width) + cells(width) * lengthCalc(lengthT) + cardTotal(width) + tableWidth(width)
-
-val meshDefault: String = tableWidth() + cardTotal(widthDefault) + cells() * lengthCalc(lengthDefault) + cardTotal(widthDefault) + tableWidth()
-
+val mesh: String = tableWidth(widthParam) + cardTotal(widthParam) + cells(widthParam)*length(lengthParam) + cardTotal(widthParam) + tableWidth(widthParam)
+val meshDefault: String = tableWidth() + cardTotal(widthDefault) + cells()*length(lengthDefault) + cardTotal(widthDefault) + tableWidth()
