03348d598f56c5e4cc415fd44ac7aa6b2b53d955
hanschi02 <hannah_monne@icloud.com>
2022-05-13 10:55:50 +0200

    Update Test

--- src/test/scala/BlackJack/model/PlayerSpec.scala
+++ src/test/scala/BlackJack/model/PlayerSpec.scala
@@ -4,11 +4,19 @@ package model
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers._
 
-class PlayerSpec {
-//    val p = Player("Test", )
-//    "Player" should {
-//      "Return Player scoreboardline " in {
-//        p.playerScore() should be ("Test 0")
-//      }
-//    }
+class PlayerSpec extends AnyWordSpec {
+  val deckPlayer = Deck()
+  val cardPlayer = Card(Suit.Spade, Rank.Queen)
+  val player = Player("Bernhardt", 0, deckPlayer)
+  "Player" should {
+    "be able to change his score" in {
+      player.changeScore(21) should be (21)
+    }
+    "have a name" in {
+      player.toString() should be("Bernhardt")
+    }
+    "be able to add a Card to the top of the Deck" in {
+      player.addToDeck(cardPlayer) should be (deckPlayer.addToTop(cardPlayer))
+    }
+  }
 }
